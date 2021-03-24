9617650c4354ca9172d8c4e7bd459777e205da60
Ahmed Razavi <ahmedrazavi92@gmail.com>
2021-03-25 00:42:21 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
+const PouchDB = require('pouchdb');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, 'public')), require('express-pouchdb')(PouchDB))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
