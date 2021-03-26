c62b7846421c885eeda1a56fa936f5cee57b0d17
Ahmed Razavi <ahmedrazavi92@gmail.com>
2021-03-26 23:29:15 +0500

    Install cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const PouchDB = require('pouchdb');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 express()
   .use(express.static(path.join(__dirname, 'public')), require('express-pouchdb')(PouchDB))
+  .use(cors())
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
