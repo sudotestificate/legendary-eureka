520d0f0c9d96731eba45d589b94c177e9bf1eb1c
Baghdad Raza <baghdadraza1998@gmail.com>
2022-05-26 17:17:48 +0500

    organize database

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 
+let database = PouchDB.defaults({
+  prefix: './database/pouchdb/dbs/',
+})
+
 express()
-  .use(express.static(path.join(__dirname, 'public')), require('express-pouchdb')(PouchDB))
+  .use(
+    express.static(path.join(__dirname, 'public')), 
+    require('express-pouchdb')(database, {
+      logPath: './database/pouchdb/logs/log.txt',
+      configPath: './database/pouchdb/config.json',
+    })
+  )
   .use(cors())
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
