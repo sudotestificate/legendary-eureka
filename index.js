f6bf54a1742a405035962986390a6d5c4b83c020
Baghdad Raza <baghdadraza1998@gmail.com>
2022-06-04 06:08:56 +0500

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let database = PouchDB.defaults({
 express()
   .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:9000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
