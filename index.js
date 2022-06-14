41466f9552cd3d207d4da07afee5f761f1c22706
baghdadraza <71591172+baghdadraza@users.noreply.github.com>
2022-06-14 15:49:45 +0500

    origin all

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ let database = PouchDB.defaults({
 
 express()
   .use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   
