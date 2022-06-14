a44abb87bf4b9392ffda99ddb34b0cdf5965a206
baghdadraza <71591172+baghdadraza@users.noreply.github.com>
2022-06-14 16:19:09 +0500

    origin all test

--- index.js
+++ index.js
@@ -10,12 +10,16 @@ let database = PouchDB.defaults({
 
 express()
   .use(function(req, res, next) {
+    
+    // Allow every domain
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
 
-    const allowedOrigins = ['http://localhost:9000', 'http://192.168.43.29:9000']
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    // Whitlist
+    // const allowedOrigins = ['http://localhost:9000']
+    // const origin = req.headers.origin;
+    // if (allowedOrigins.includes(origin)) {
+    //   res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
 
     // res.header("Access-Control-Allow-Origin", "*");
     // res.header("Access-Control-Allow-Origin", "http://localhost:9000");
